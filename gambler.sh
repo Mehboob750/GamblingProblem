92dedf27ca42c39234efee7f0e670d339c08a0c4
unknown <ms568203@gmail.com>
2020-04-19 07:50:14 +0530

    Refactor. Added usecase3-if Gambler win or lose 50% of the stake would resign for the day

--- gambler.sh
+++ gambler.sh
@@ -1,14 +1,24 @@
 #!/bin/bash -x
 stake=100;
 bet=1;
-
 gamblerWin=1;
 gamblerLose=0;
-result=$((RANDOM%2));
-if (( $result == $gamblerWin ))
-then
-	stake=$(($stake+$bet));
-else
-	stake=$(($stake-$bet));
-fi
+
+winingStake=$(($stake+($stake/2)));
+loseingStake=$(($stake-($stake/2)));
+
+while [ true ]
+do
+	if (( $stake==$winingStake || $stake==$loseingStake ))
+	then
+		break;
+	fi
+	result=$((RANDOM%2));
+	if (( $result == $gamblerWin ))
+	then
+		stake=$(($stake+$bet));
+	else
+		stake=$(($stake-$bet));
+	fi
+done
 #echo "Gambler Stake : " $stake;
