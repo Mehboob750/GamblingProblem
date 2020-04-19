1cc4433881a37b18d9eee26d31d01f379ed8f51b
unknown <ms568203@gmail.com>
2020-04-19 07:24:38 +0530

    Refactor. Added Usecase2- Gambler make bet so either Win or Lose

--- gambler.sh
+++ gambler.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 stake=100;
 bet=1;
+
+gamblerWin=1;
+gamblerLose=0;
+result=$((RANDOM%2));
+if (( $result == $gamblerWin ))
+then
+	stake=$(($stake+$bet));
+else
+	stake=$(($stake-$bet));
+fi
+#echo "Gambler Stake : " $stake;
