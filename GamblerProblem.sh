74f7ca80bdc84a522369b4daf0d176561e4f67fd
unknown <ms568203@gmail.com>
2020-04-20 18:16:37 +0530

    Refactor. Added usecase7- asked for continue playing for next month or stop

--- GamblerProblem.sh
+++ GamblerProblem.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -x
 numberOfDays=20;
-stake=0;
-everyDayStake=100;
 bet=1;
 gamblerWin=1;
 gamblerLose=0;
@@ -9,46 +7,59 @@ declare -A winForTheDay;
 declare -A loseForTheDay;
 iteration1=0;
 iteration2=0;
-
-for(( iteration=1; iteration<=numberOfDays; iteration++ ))
+while [ true ]
 do
-   stake=$(($stake+$everyDayStake));
-   winingStake=$(($stake+($stake/2)));
-   loseingStake=$(($stake-($stake/2)));
-   while [ true ]
-   do
-      if (( $todaysStake==$winingStake || $todaysStake==$loseingStake ))
-      then
-         break;
-      fi
 
-      playBet=$((RANDOM%2));
+	stake=0;
+	stakePerDay=100;
+	for(( iteration=1; iteration<=numberOfDays; iteration++ ))
+	do
+   		stake=$(($stake+$stakePerDay));
+   		winingStake=$(( $stake + ($stake/2) ));
+   		loseingStake=$(( $stake - ($stake/2) ));
+   		while [ true ]
+   		do
+      		if (( $todaysStake==$winingStake || $todaysStake==$loseingStake ))
+      		then
+         		break;
+      		fi
 
-      if (( $playBet == $gamblerWin ))
-      then
-         todaysStake=$(($todaysStake+$bet));
-      else
-         todaysStake=$(($todaysStake-$bet));
-      fi
-   done
+      		result=$((RANDOM%2));
 
-   if (( $stake < $todaysStake ))
-   then
-      winForTheDay[Day-"$iteration"]=$(($todaysStake-$stake));
-   else
-      loseForTheDay[Day-"$iteration"]=$(($stake-$todaysStake));
-   fi
+      		if (( $result == $gamblerWin ))
+      		then
+         		todaysStake=$(($todaysStake+$bet));
+      		else
+         		todaysStake=$(($todaysStake-$bet));
+     		fi
+   		done
 
-   stake=$todaysStake;
-   #sleep 5s;
-   #everyDayResult[((iteration))]="$stake";
-   #sleep 5s;
+   		if (( $stake < $todaysStake ))
+   		then
+      		winForTheDay[Day-"$iteration"]=$(($todaysStake-$stake));
+   		else
+      		loseForTheDay[Day-"$iteration"]=$(($stake-$todaysStake));
+   		fi
+
+   		stake=$todaysStake;
+   		#sleep 5s;
+   		#everyDayResult[((iteration))]="$stake";
+   		#sleep 5s;
+	done
+	echo "Gambler Total Stake After 20 days : " $stake;
+	#echo ${everyDayResult[@]};
+	echo "Win for the days: " ${winForTheDay[@]};
+	echo ${!winForTheDay[@]};
+
+	echo "Lose for the days: " ${loseForTheDay[@]};
+	echo ${!loseForTheDay[@]};
+
+	resultMonth[((Counter1++))]="$stake";
+	echo ${resultMonth[@]};
+	read -p "Enter 1 for play for next month/ Enter 0 to stop : " choice;
+	if (( $choice == 0 ))
+	then
+			break;
+	fi
+
 done
-#echo "Gambler Stake : " $stake;
-#echo ${everyDayResult[@]};
-echo "Win for the days: " ${winForTheDay[@]};
-echo ${!winForTheDay[@]};
-
-echo "Lose for the days: " ${loseForTheDay[@]};
-echo ${!loseForTheDay[@]};
-echo "Total amount after 2o days are: " $stake;
